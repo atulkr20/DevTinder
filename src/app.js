d824750f5d9301861c182f580df8660748693197
atulkr20 <atulkrjha59@gmail.com>
2025-07-04 13:44:58 +0530

    Played with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,15 +2,52 @@ const express = require('express')
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params)
-    res.send({firstName: "Atul", lastName: "jha"})
-})
-
-
-app.use((req, res) =>  // This is a Request Handler
-    res.send('Hello i am your sever and i am working totally fine')
+app.get(
+    "/user",
+    (req, res, next) => {
+        console.log("This is running successfully on 1")
+        next();
+        res.send("1st response!!")
+    }
 )
+
+app.get (
+    "/user", 
+    (req, res, next) => {
+        console.log("Server is successfully running on 2 ")
+        next();
+       // res.send("2nd response!!")
+    }
+)
+
+app.get (
+    "/user", 
+    (req, res, next) => {
+        console.log("Server is successfully running on 3 ")
+        next();
+       // res.send("3rd response!!")
+    }
+)
+
+app.get(
+    "/user",
+    (req, res, next) => {
+        console.log("Server is successfully running on 4")
+            next();
+           // res.send("4th response!!")
+        }
+    
+)
+
+app.get(
+    "/user",
+    (req, res, next) => {
+        console.log("Server is successfully running on 5")
+        res.send("5th response")
+    }
+)
+
+
 
 app.listen(3000, () => {
 console.log('Sever is successfully running on server 30000')

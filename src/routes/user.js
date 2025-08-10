6c88a738f251c00cc61bf8a7d55ba5a0e49e9f45
Atul Kumar Jha <atuljha21542@gmail.com>
2025-08-10 19:04:40 +0530

    Created Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -54,7 +54,35 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 });
 
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try{
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [{fromUserId: loggedInUser._id}, {toUserId: loggedInUser._id}],
+        }).select("fromUserId", "toUserId");
 
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach(req => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+        
+        const users = await User.find({
+            $and: [
+           { _id: {$nin: Array.from(hideUsersFromFeed) }},
+            {_id: {$ne: loggedInUser._id}},
+            ], 
+        }).select(USER_SAFE_DATA);
+        res.send(connectionRequests);
+
+
+
+        res.send(conectionRequests);
+    }catch(err) {
+        res.status(400).send("ERROR! " + err.message)
+    }
+
+});
 
 
 module.exports = userRouter;

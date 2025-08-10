b3fb988d8d63b12c104f4b0d5512a3c14d8b4512
Atul Kumar Jha <atuljha21542@gmail.com>
2025-08-10 19:25:53 +0530

    Does the Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -57,8 +57,17 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
     try{
         const loggedInUser = req.user;
-        const connectionRequests = await ConnectionRequest.find({
-            $or: [{fromUserId: loggedInUser._id}, {toUserId: loggedInUser._id}],
+         
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit >50 ? 50 : limit;
+        
+        const skip = (page-1)*limit;
+
+      const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id}, 
+                {toUserId: loggedInUser._id}],
         }).select("fromUserId", "toUserId");
 
         const hideUsersFromFeed = new Set();
@@ -72,12 +81,9 @@ userRouter.get("/feed", userAuth, async (req, res) => {
            { _id: {$nin: Array.from(hideUsersFromFeed) }},
             {_id: {$ne: loggedInUser._id}},
             ], 
-        }).select(USER_SAFE_DATA);
-        res.send(connectionRequests);
-
-
-
-        res.send(conectionRequests);
+        }).select(USER_SAFE_DATA).skip().limit(limit);
+        res.send(users);
+    
     }catch(err) {
         res.status(400).send("ERROR! " + err.message)
     }

6fc83629e321da5575e228ff96f466e9e22de022
Atul Kumar Jha <atuljha21542@gmail.com>
2025-08-11 14:51:51 +0530

    Added footer and Body component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+import NavBar from "./NavBar"; 
+import Footer from "./Footer";
+
 
 function App() {
   return (
     <>
-    <NavBar />
+
+    <BrowserRouter basename = "/">
+          <NavBar />
+
+    <Routes>
+      <Route path="/" element={<Body />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Profile" element={<Profile />} />
+    </Routes>
+    
+    <Footer />
+    </BrowserRouter>
 <h1 className="text-3xl font-bold text-blue-500">Hello DevTinder!</h1>
     </>
   );

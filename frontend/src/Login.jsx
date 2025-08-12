40e92a8463040b1445da085121a616d32036f6ca
Atul Kumar Jha <atuljha21542@gmail.com>
2025-08-12 23:56:45 +0530

    Changed some code of Login Page as per Daisy UI

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 
 const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("Email:", emailId);
+    console.log("Password:", password);
+  };
+
   return (
     <div className="flex justify-center">
       <div className="card bg-base-300 w-96 shadow-sm">
         <div className="card-body">
           <h2 className="card-title justify-center">Login</h2>
-          
+
           {/* Email Field */}
           <fieldset className="fieldset">
             <legend className="fieldset-legend">Email ID</legend>
             <input
               type="email"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
               className="input input-bordered w-full"
               placeholder="Enter your email"
             />
@@ -21,6 +32,8 @@ const Login = () => {
             <legend className="fieldset-legend">Password</legend>
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="input input-bordered w-full"
               placeholder="Enter your password"
             />
@@ -28,7 +41,12 @@ const Login = () => {
 
           {/* Login Button */}
           <div className="card-actions justify-center my-2">
-            <button className="btn btn-primary">Login</button>
+            <button
+              onClick={handleLogin}
+              className="btn btn-primary w-full"
+            >
+              Login
+            </button>
           </div>
         </div>
       </div>
